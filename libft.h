6b9bf2c96e9d2d4eeec0dbc6ed176d2879b042af
Michael Ratke <mratke@2-G-4.42heilbronn.de>
2024-10-07 21:21:32 +0200

    First commit! Hello 42) Enviroment is setted, ft_isalpha and ft_isdigit with tests done, added header libft.h

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 20:17:40 by mratke            #+#    #+#             */
+/*   Updated: 2024/10/07 21:16:39 by mratke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+//includes
+# include <stdlib.h>
+# include <stdio.h>
+
+// Part 1 - Libc functions
+int	ft_isalpha(int ch);
+int	ft_isdigit(int ch);
+
+#endif
