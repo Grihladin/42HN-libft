833c156452bca37941f2fa945aaf408f75128aaa
Michael Ratke <mratke@2-H-2.42heilbronn.de>
2024-10-08 12:43:50 +0200

    Add strlen now event time xD

---
+++ ft_strlen.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 12:39:03 by mratke            #+#    #+#             */
+/*   Updated: 2024/10/08 12:42:27 by mratke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+// void	test_ft_strlen(void)
+// {
+// 	int	result;
+
+// 	// Test case 1: Empty string
+// 	result = ft_strlen("");
+// 	if (result == 0)
+// 		printf("Test case 1 passed!\n");
+// 	else
+// 		printf("Test case 1 failed!\n");
+
+// 	// Test case 2: String with length 5
+// 	result = ft_strlen("Hello");
+// 	if (result == 5)
+// 		printf("Test case 2 passed!\n");
+// 	else
+// 		printf("Test case 2 failed!\n");
+
+// 	// Test case 3: String with spaces
+// 	result = ft_strlen("Hello, World!");
+// 	if (result == 13)
+// 		printf("Test case 3 passed!\n");
+// 	else
+// 		printf("Test case 3 failed!\n");
+
+// 	// Test case 4: String with special characters
+// 	result = ft_strlen("\t\n@!");
+// 	if (result == 4)
+// 		printf("Test case 4 passed!\n");
+// 	else
+// 		printf("Test case 4 failed!\n");
+// }
+
+// int	main(void)
+// {
+// 	test_ft_strlen();
+// 	return (0);
+// }
