2713b0b0de812b9371956ca1d4ad2a7222f59639
Michael Ratke <mratke@1-C-25.42heilbronn.de>
2024-10-08 12:01:43 +0200

    Made 5 functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:17:40 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/07 21:16:39 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/08 11:57:55 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@
 // Part 1 - Libc functions
 int	ft_isalpha(int ch);
 int	ft_isdigit(int ch);
+int	ft_toupper(int ch);
+int	ft_tolower(int ch);
+int	ft_isalnum(int ch);
+int	ft_isascii(int c);
+int	ft_isprint(int ch);
 
 #endif
