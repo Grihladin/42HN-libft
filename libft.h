f0b20acb7c27f361ee405243bfa6a8fdc9e60c2a
Michael Ratke <mratke@2-F-2.42heilbronn.de>
2024-10-09 13:03:31 +0200

    added mote functions

--- libft.h
+++ libft.h
@@ -6,24 +6,29 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:17:40 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/08 11:57:55 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/09 13:00:35 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-//includes
-# include <stdlib.h>
+// includes
 # include <stdio.h>
+# include <stdlib.h>
 
 // Part 1 - Libc functions
-int	ft_isalpha(int ch);
-int	ft_isdigit(int ch);
-int	ft_toupper(int ch);
-int	ft_tolower(int ch);
-int	ft_isalnum(int ch);
-int	ft_isascii(int c);
-int	ft_isprint(int ch);
+int		ft_isalpha(int ch);
+int		ft_isdigit(int ch);
+int		ft_toupper(int ch);
+int		ft_tolower(int ch);
+int		ft_isalnum(int ch);
+int		ft_isascii(int c);
+int		ft_isprint(int ch);
+int		ft_strlen(char *str);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strrchr(const char *str, int ch);
+int		ft_strcmp(const char *lhs, const char *rhs);
+void	*ft_memset(void *dest, int ch, size_t count);
 
 #endif
