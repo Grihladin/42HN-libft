001cc131b8493cd7344b9fc00712619837bd493f
mratke <mratke@student.42heilbronn.de>
2024-10-11 13:01:34 +0200

    strlcpy improoved naming

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,22 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 10:59:19 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/10 12:41:36 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/11 12:59:08 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size)
 {
 	size_t	i;
 
 	i = 0;
-	if (size == 0)
+	if (dest_size == 0)
 	{
 		return (ft_strlen(src));
 	}
-	while (i < size - 1 && src[i] != '\0')
+	while (i < dest_size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
