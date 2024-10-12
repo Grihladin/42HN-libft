3231ef4c665736575461cce53ffcd42febc78138
mratke <mratke@student.42heilbronn.de>
2024-10-12 16:16:25 +0200

    OMG i found it and it was jsut a 1 sting, why dont people own infinite RAM

--- ft_substr.c
+++ ft_substr.c
@@ -6,16 +6,9 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 21:07:38 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/11 16:37:40 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/12 15:09:40 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// PROBLEMS
-// 1
-// For example, if len = 10,
-// 	but the string from position start has only 5 characters left,
-// 	you are still allocating space for 10 characters, which is wasteful.
-//
 
 #include "libft.h"
 
@@ -27,16 +20,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 
 	s_len = ft_strlen(s);
-	if (start > (unsigned int)s_len)
+	if (start >= s_len)
 		len = 0;
-	if (s_len < len)
-		len = ft_strlen(s);
+	if (s_len - start < len)
+		len = s_len - start;
 	result = malloc((len + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 	i = 0;
 	j = (size_t)start;
-	while (i < len && s[j] != '\0')
+	while (i < len)
 	{
 		result[i] = s[j];
 		i++;
@@ -56,7 +49,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 // 	i = 0;
 // 	str = "01234";
 // 	size = 10;
-// 	ret = ft_substr(str, 10, size);
+// 	ret = ft_substr("", 1, 1);
 // 	i = 0;
 // 	while (ret[i] != '\0')
 // 	{
