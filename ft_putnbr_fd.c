b850fe9dd940ef4ed6e69394d5b77c1127ae1b71
mratke <mratke@student.42heilbronn.de>
2024-10-13 20:11:20 +0200

    deleted widlcard form makefile, made putnbr_fd more readable

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 18:20:46 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/13 18:28:37 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/13 20:10:08 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*n_string;
+	int	mod;
+	int	div;
 
-	n_string = ft_itoa(n);
-	write(fd, n_string, ft_strlen(n_string));
-	free(n_string);
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		write(fd, "-", 1);
+	}
+	div = n / 10;
+	mod = n % 10;
+	if (n > 9)
+	{
+		ft_putnbr_fd(div, fd);
+	}
+	ft_putchar_fd((mod) + '0', fd);
 }
