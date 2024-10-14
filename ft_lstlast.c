6d342c1007b6217b4cfabea85ad6b891c60f5a22
mratke <mratke@student.42heilbronn.de>
2024-10-14 22:23:51 +0200

    OMG i would never figure it out, thx Abdulagit add .git add . didnt free the last result of function form parametr. Also improoved lstlast a bit to prevet possible leaks

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:09:13 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/14 21:42:38 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/14 21:45:11 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*current;
 
-	current = lst;
 	if (lst != NULL)
 	{
+		current = lst;
 		while (current->next != NULL)
 		{
 			current = current->next;
