59eccbec4fc52020f603b370e1199c6149568cfb
mratke <mratke@student.42heilbronn.de>
2024-10-14 15:47:44 +0200

    ok i dealed with first two bonus function and all this confusing pointer stuff

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 20:45:12 by mratke            #+#    #+#             */
+/*   Updated: 2024/10/14 14:07:57 by mratke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	new_node->next = NULL;
+	new_node->content = content;
+	return (new_node);
+}
